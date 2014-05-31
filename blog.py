163863ea2031bb6c251ef410570506ad31b42332
Hunter Thornsberry <hunter@hunterthornsberry.com>
2014-05-31 04:57:12 -0400

    Updated

--- blog.py
+++ blog.py
@@ -9,14 +9,13 @@ else:
 date = (time.strftime("%d/%m/%Y %H:%M:%S"))
 
 filename = raw_input("File name? ")
-#print postdir + filename
 title = raw_input("Page Title? ")
 content = raw_input("Page content? ")
 author = raw_input("Author? ")
 
 # Open a file
 fo = open(postdir + filename, "wb")
-fo.write("<html><head><title=" + title + "></head><body><h1>" + title + " - " + date +"</h1>" + content + "<br><br>" + "-" + author);
+fo.write("<html><head><title=" + title + "></head><body><h1>" + title + " - " + date +"</h1><h3><a href='index.html'>Back to Blog</a></h3>" + content + "<br><br>" + "-" + author);
 
 # Close opend file
 fo.close()
@@ -28,7 +27,6 @@ lines.insert(8, "<a href='" + postdir + filename + "'>" + title + " - " + date +
 point = 0
 while point < len(lines):
 	lines[point] = lines[point] + "\n"
-	#print lines[point]
 	point = point + 1
 
 with open('index.html', 'w') as file:
